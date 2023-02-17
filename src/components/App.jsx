3167f12c5727718f3876157f2b3befda3f9b6b8e
Yuliia Koriavets <112959119+YuliiaKoriavets@users.noreply.github.com>
2023-02-17 15:14:16 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
         />
         <Route
           path="login"
-          element={
+          index element={
             <PublicRoute redirectTo="/contacts" component={<LoginPage />} />
           }
         />
